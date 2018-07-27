8b82082e12f04fc24e2123681f9cd5828d4b08a9
TOTOzigemm <bowdishaw@yahoo.com>
2018-07-26 23:26:47 -0400

    test
    tickle

---
+++ scripts/bingo.js
@@ -0,0 +1,177 @@
+var bingoList = [];
+// Summer Forest 1
+bingoList[1] = [
+  { name: "100% Artisans Inventory", types: [] },
+  { name: "300 gems in Sunny Beach, Skelos Badlands and Scorch", types: [] },
+  { name: "100% Mushroom and Country Speedway", types: [] },
+  { name: "300 gems in Glimmer, Ocean Speedway and Metropolis", types: [] },
+  { name: "6 combined orbs in Hurricos, Breeze Harbor and Fracture Hills", types: [] },
+  { name: "All orbs in Idol Springs, all gems in Scorch and Fracture Hills talisman", types: [] }
+];
+bingoList[2] = [
+  { name: "10 combined eggs in Sunny Villa and Seashell Shore", types: [] },
+  { name: "All Sunrise Spring skillpoints", types: [] },
+  { name: "All powerup types", types: [] },
+  { name: "All Dragon Shores tokens", types: [] },
+  { name: "All gems in Sgt. Byrd's Base, Dr. Shemp and Gnorc Cove", types: [] },
+  { name: "7 combined orbs in Aquaria Towers, Robotica Farms and Magma Cone", types: [] }
+];
+bingoList[3] = [
+  { name: "100% Hill levels", types: [] },
+  { name: "Unlock Fireworks Factory", types: [] },
+  { name: "100% Toasty, Glimmer and Magma Cone", types: [] },
+  { name: "All dragons in Jacques, all orbs in Glimmer and all eggs in Country Speedway", types: [] }
+];
+bingoList[4] = [
+  { name: "700 combined gems in Scorch and Molten Crater", types: [] },
+  { name: "1200 Sunrise Spring gems", types: [] },
+  { name: "100% Ocean and Mushroom Speedway", types: [] },
+  { name: "Collect 10 different keys as Spyro", types: [] }
+];
+bingoList[5] = [
+  { name: "100% Sunny levels", types: [] },
+  { name: "Ignite 10 rockets", types: [] },
+  { name: "Get eaten by 3 different types of things", types: [] },
+  { name: "900 combined gems in Gnorc Cove, Sunny Beach and Seashell Shore", types: [] },
+  { name: "2 Dragons in Lofty Castle, 2 orbs in Magma Cone and 4 Eggs in Bamboo Terrace", types: [] }
+];
+// 
+bingoList[6] = [
+  { name: "100% Cloud Spires, Cloud Temples and Blowhard", types: [] },
+  { name: "75 gems in Dark Hollow, 300 gems in Ice Cavern and 350 gems in Tree Tops", types: [] },
+  { name: "Bluto, Sleepyhead and Twin Dragons", types: [] },
+  { name: "100% Peace Keepers Inventory", types: [] },
+  { name: "100% all Spyro 2 Homeworlds", types: [] },
+  { name: "Exit through 15 vortices", types: [] }
+];
+bingoList[7] = [
+  { name: "All eggs in Icy Peak and all gems in Molten Crater", types: [] },
+  { name: "1000 combined gems in Sheila's Alp, Sgt. Byrd's Base and Bentley's Outpost", types: [] },
+  { name: "250 gems in Idol Springs, Zephyr and Mystic Marsh", types: [] },
+  { name: "All enemy particles in 9 levels", types: [] },
+  { name: "All boss skill points and unlock all Spyro 2 speedways", types: [] },
+  { name: "1200 Summer Forest gems", types: [] }
+];
+bingoList[8] = [
+  { name: "All gems in Sheila's Alp and Mystic Marsh", types: [] },
+  { name: "Puzzle and both Blind Guys", types: [] },
+  { name: "1600 Midday Gardens gems", types: [] },
+  { name: "10 Hunter orbs/eggs", types: [] }
+];
+bingoList[9] = [
+  { name: "100% Town levels", types: [] },
+  { name: "100% Farm levels", types: [] },
+  { name: "2100 Evening Lake gems", types: [] },
+  { name: "Freeze something in 2 levels", types: [] }
+];
+bingoList[10] = [
+  { name: "75 gems in Dark Hollow, 300 gems in Zephyr and 375 gems in Spooky Swamp", types: [] },
+  { name: "300 gems in Terrace Village, Canyon Speedway and Sheila's Alp", types: [] },
+  { name: "Red Spring Chest, Stand on 10 Avalar Portals and Save Noodles", types: [] },
+  { name: "100% Terrace Village, Bamboo Terrace and Cloud Temples", types: [] },
+  { name: "20 Strong containers", types: [] }
+];
+// 
+bingoList[11] = [
+  { name: "All eggs in Cloud Spires and all gems in Frozen Altars", types: [] },
+  { name: "Sun Seeds, Sand Castle and Jack's Beans", types: [] },
+  { name: "100% Magic Crafters Inventory", types: [] },
+  { name: "All orbs in Breeze Harbor, all gems in Shady Oasis and Colossus talisman", types: [] },
+  { name: "Dreak 18 fan chests", types: [] },
+  { name: "All Spyro 1 eggs", types: [] }
+];
+bingoList[12] = [
+  { name: "5 Skateboarding eggs", types: [] },
+  { name: "100% Ridge levels", types: [] },
+  { name: "Kill all heavily armored enemies in High Caves and Haunted Towers", types: [] },
+  { name: "Spell BINGO with Baby Dragons", types: [] },
+  { name: "All eggs in Sgt. Byrd's Base and all gems in Bentley's Outpost", types: [] },
+  { name: "Kill an enemy with a cannon in each game", types: [] }
+];
+bingoList[13] = [
+  { name: "#BoneDances", types: [] },
+  { name: "100% 2 Harbor levels", types: [] },
+  { name: "Pay Moneybags 10 times", types: [] }
+];
+// 
+bingoList[14] = [
+  { name: "Farley and George", types: [] },
+  { name: "All Hockey Challenges", types: [] },
+  { name: "7 2-part challenges", types: [] }
+];
+bingoList[15] = [
+  { name: "All gems in Gnorc Gnexus, Midday Gardens and Autumn Plains", types: [] },
+  { name: "100% Icy levels", types: [] },
+  { name: "100% Icy Peak, Wizard Peak and Colossus", types: [] },
+  { name: "100% Tower levels", types: [] },
+  { name: "30 Fireworks containters", types: [] }
+];
+// 
+bingoList[16] = [
+  { name: "All Bentley Side Area eggs", types: [] },
+  { name: "100% Beast Makers Inventory", types: [] },
+  { name: "4 skillpoints in Summer Forest levels", types: [] },
+  { name: "All orbs in Summer Forest and Shady Oasis", types: [] },
+  { name: "All dragons in Ice Cavern and Dark Passage", types: [] },
+  { name: "12 combined dragons in Misty Bog, Dark Passage and Alpine Ridge", types: [] },
+];
+bingoList[17] = [
+  { name: "Use all invincibility powerups", types: [] },
+  { name: "All Evening Lake Skillpoints", types: [] },
+  { name: "5 skillpoints in Autumn Plains levels", types: [] },
+  { name: "1500 Autumn Plains gems", types: [] },
+  { name: "100% Flight levels", types: [] },
+  { name: "100% 5 boss levels", types: [] }
+];
+bingoList[18] = [
+  { name: "All gems in 6 speedways", types: [] },
+  { name: "5 Mystery Jars", types: [] },
+  { name: "20 orbs/eggs from minigames or sublevels", types: [] }
+];
+bingoList[19] = [
+  { name: "15 skillpoints", types: [] },
+  { name: "All Handel Collectibles", types: [] },
+  { name: "Kill 15 thieves", types: [] }
+];
+bingoList[20] = [
+  { name: "All gems in Honey Speedway, Winter Tundra Homeworld and Metalhead", types: [] },
+  { name: "Defeat Gnasty Gnorc, Ripto or the Sorceress", types: [] },
+  { name: "Supercharge in 7 levels", types: [] },
+  { name: "Break 25 life containers", types: [] },
+  { name: "Use 40 different whirlwinds", types: [] }
+];
+// 
+bingoList[21] = [
+  { name: "750 combined gems in Desert Ruins and Crystal Islands", types: [] },
+  { name: "9 combined eggs in Desert Ruins and Crystal Islands", types: [] },
+  { name: "Enter Agent 9's Lab", types: [] },
+  { name: "4 Side Character skillpoints", types: [] },
+  { name: "All gems in Metropolis and 100% Metro Speedway", types: [] },
+  { name: "100% Dream Weavers Inventory", types: [] }
+];
+bingoList[22] = [
+  { name: "All orbs in Zephyr, All gems in Mystic Marsh and Aquaria Towers", types: [] },
+  { name: "All orbs in Robotica Farms and Metropolis", types: [] },
+  { name: "30 Dragons", types: [] },
+  { name: "3 speedway eggs", types: [] },
+  { name: "1337 Winter Tundra gems", types: [] },
+  { name: "100% a level in each homeworld in Spyro 1", types: [] }
+];
+bingoList[23] = [
+  { name: "All Seaweed skillpoints and Manta challenges", types: [] },
+  { name: "7500 gems", types: [] },
+  { name: "225 gems in Idol Springs and Agent 9's Lab", types: [] }
+];
+bingoList[24] = [
+  { name: "All dragons in Twilight Harbor, all orbs in Crystal Glacier and Hurricos", types: [] },
+  { name: "Collect 60 Dragons/Orbs/Named Eggs", types: [] },
+  { name: "300 gems in Gnorc Cove, Hurricos and Icy Speedway", types: [] }
+];
+bingoList[25] = [
+  { name: "300 gems in Twilight Harbor, Metro Speedway and Honey Speedway", types: [] },
+  { name: "100% Crystal levels", types: [] },
+  { name: "100% Canyon Speedway, Dry Canyon and Desert Ruins", types: [] },
+  { name: "Defeat 8 bosses", types: [] }
+];
+
+$(function() { srl.bingo(bingoList, 5); });
